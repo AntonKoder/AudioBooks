3f36ee540427a07e5df56a55bfc56c4227bfec94
Anton Sharapov <4nt0n64r@gmail.com>
2021-09-03 11:07:04 +0200

    Initial commit
    Добавлены зависимости в build.gradle. Подключен detekt.

---
+++ app/src/main/java/com/a4nt0n64r/audiobooks/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.a4nt0n64r.audiobooks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
